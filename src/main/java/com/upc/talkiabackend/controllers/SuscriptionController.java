78bdc28d8c0b67daf8016a37501bd45c7d34001c
CamiTorres22 <u202217059@upc.edu.pe>
2024-10-14 15:03:42 -0500

    feat(backend): Add Implement security with token verification to visualize all suscriptions

--- src/main/java/com/upc/talkiabackend/controllers/SuscriptionController.java
+++ src/main/java/com/upc/talkiabackend/controllers/SuscriptionController.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class SuscriptionController {
 
     private final ModelMapper modelMapper = new ModelMapper();
 
-   // @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/suscription")
     public ResponseEntity<?> listSuscription(){
         try {
