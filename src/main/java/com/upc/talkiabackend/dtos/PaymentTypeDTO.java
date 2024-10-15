d4919f209119e85bc31e519366070b2a0d7db0b9
alvarez.silvanaa <silvanaach92@gmail.com>
2024-10-15 11:15:45 -0500

    feat(backend): Add Create the DTO for PaymentType and adapt it to the endpoint

---
+++ src/main/java/com/upc/talkiabackend/dtos/PaymentTypeDTO.java
@@ -0,0 +1,15 @@
+package com.upc.talkiabackend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentTypeDTO {
+    private Integer id;
+    private String name;
+}
