43e0439d10ec6fdfdeebafd3e4e179bd597512db
alvarez.silvanaa <silvanaach92@gmail.com>
2024-10-16 00:38:03 -0500

    feat(backend): Add endpoint for admin’s verification

--- src/main/java/com/upc/talkiabackend/controllers/TestController.java
+++ src/main/java/com/upc/talkiabackend/controllers/TestController.java
@@ -2,12 +2,24 @@ package com.upc.talkiabackend.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class TestController {
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/user")
     public String userEndpoint() {
         return "Bienvenido a talkia estimado estudiante";
     }
+
+
+    @GetMapping("/admin")
+    //@PreAuthorize("hasRole('ADMIN')")
+    public String adminEndpoint() {
+        return "Bienvenido a talkia estimado administrador";
+    }
+
 
 }
