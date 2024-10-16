154c304607970afbd4870da25bdf6ce2328bc7e2
CamiTorres22 <u202217059@upc.edu.pe>
2024-10-16 00:30:31 -0500

    feat(backend): Add security at the endpoint to the user login

--- src/main/java/com/upc/talkiabackend/controllers/TestController.java
+++ src/main/java/com/upc/talkiabackend/controllers/TestController.java
@@ -1,10 +1,11 @@
 package com.upc.talkiabackend.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class TestController {
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/user")
-    //@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public String userEndpoint() {
         return "Bienvenido a talkia estimado estudiante";
     }
